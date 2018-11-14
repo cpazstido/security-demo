252e5f8fe0e6231446177b061864105d76fd084a
cpazstido <120964417@qq.com>
2018-11-14 14:40:29 +0800

    controller死循环，测试jstack

--- src/main/java/com/cf/security/controller/AppController.java
+++ src/main/java/com/cf/security/controller/AppController.java
@@ -11,4 +11,17 @@ public class AppController {
     public String appIndex(){
         return "hello app";
     }
+
+    /**
+     * 死循环
+     * @return
+     */
+    @RequestMapping("for")
+    public String fora(){
+        int i=0;
+        while(true){
+            System.out.println(i++);
+        }
+
+    }
 }
--- src/main/java/com/cf/security/controller/UserController.java
+++ src/main/java/com/cf/security/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.cf.security.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import sun.plugin.liveconnect.SecurityContextHelper;
 
 import java.security.Principal;
 import java.util.HashMap;
